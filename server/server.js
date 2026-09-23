2833dfe9b00e2c780fc2b58bbbb1f5ca4ce9da45
Bhavesh-Gadekar <bhaveshgadekar.rmdstic.comp@gmail.com>
2026-09-23 15:41:42 +0530

    Database connection

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req, res) => {
+    res.json({message: "Gym Tracker API is running"});
+});
+
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+});
